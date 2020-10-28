530563de1631afbefdcdec965d393f4951eb9111
Sebastian017795 <A01379654@itesm.mx>
2020-10-28 21:19:39 +0000

    Modidique hola.py

--- hola.py
+++ hola.py
@@ -1,3 +1,37 @@
-lista = [1,4,7,2,10]
-cs=sorted(lista)
-print(cs)
+# %%
+
+import numpy as np
+import pandas as pd
+lista = [1,2,4,6]
+nplista = np.array(lista)
+pdlista = pd.Series(lista)
+
+print(lista*2)
+print(nplista*2)
+print(pdlista*2)
+
+
+
+# %%
+df = pd.read_csv("insurance.csv")
+print(df)
+
+# %%
+df.head()
+#df.tail()
+#df.sample()
+
+# %%
+#df.info()
+df.describe()
+
+# %%
+df[['age','bmi']]
+
+# %%
+df[:2][['age','bmi']]
+
+# %%
+df[10:11]
+
+# %%
